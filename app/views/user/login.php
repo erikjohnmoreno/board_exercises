c9f3b70c5a2e6101fa1d8e5660bf84618234f70f
erikjohnmoreno <e.moreno@klab.com>
2015-02-12 14:20:44 +0800

    done login page

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -5,15 +5,15 @@
 <?php endif ?>
 <body style="background: url('/bootstrap/img/board.jpg'); background-repeat: no-repeat;">
 <center>
-<div style="padding-top: 400px; padding-left: 450px;">
-<form class="span7" method="post" action="">
+<div style="padding-top: 400px; padding-left: 400px; overflow: hidden">
+<form class="span6" method="post" action="">
     <div class="container">
         <div class="row">
-            <div class="span7">
+            <div class="span6">
                 <div class="form-login">
                     <h4>WHAT's UP DUDE?</h4>
-                    <input class="span7" type="text" id="userName" name="username" class="form-control input-sm chat-input" placeholder="username" />
-                    <input class="span7" type="password" id="userPassword" name="password" class="form-control input-sm chat-input" placeholder="password" />
+                    <input class="span6" type="text" id="userName" name="username" class="form-control input-sm chat-input" placeholder="username" />
+                    <input class="span6" type="password" id="userPassword" name="password" class="form-control input-sm chat-input" placeholder="password" />
                     <input type="hidden" name="page_next" value="login_next">
                     </br>
                     <div class="wrapper">
